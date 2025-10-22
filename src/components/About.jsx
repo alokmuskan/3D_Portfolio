4fa032c85b846611501509f1b0d8162d57b142aa
alok raj <alokraj1319@gmail.com>
2025-10-23 01:52:34 +0530

    building About Section in which done with Overview of Introduction and can change it from index.js of constants

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,56 @@
 import React from "react";
-import Tilt  from "react-tilt";
+import { Tilt } from 'react-tilt';
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <p>{title}</p>
+  )
+}
+
 const About = () => {
   return (
     <>
-    
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}
+        >Introduction</p>
+        <h2 className={styles.sectionHeadText}
+        >Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "",0.1, 1)}
+        className="mt-4 text-secondary text-[17px]
+        max-w-3-xl leading-[30px]"
+      >
+        A passionate and curious developer who 
+        loves transforming ideas into real-world 
+        solutions through code. With a strong 
+        foundation in C++ and a growing command 
+        over Data Structures and Algorithms, I 
+        enjoy solving logical problems and writing 
+        efficient, clean code.My journey into 
+        technology started with an interest in how 
+        software works behind the scenes, which led 
+        me to explore web development. I ve built 
+        several projects using HTML, CSS, and JavaScript, 
+        focusing on creating responsive, user-friendly 
+        designs and seamless interfaces. I m continuously 
+        improving my front-end development skills while 
+        also exploring back-end technologies to become a 
+        well-rounded developer.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index=
+          {index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
