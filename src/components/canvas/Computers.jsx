d7a1bccd013b689975465e0a4848ff2ae2a35067
alok raj <alokraj1319@gmail.com>
2025-10-22 23:39:46 +0530

    created canvas for 3D model and the computer 3D model is visible but it needs some more styling

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Suspense, useEffect, useState} from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import { CanvasLoader } from "../Loader";
+import CanvasLoader from '../Loader.jsx';
 
 const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf")
@@ -20,5 +20,26 @@ const Computers = () => {
   )
 }
 
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25}}
+      gl={{ preserveDrawingBuffer: true }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <Computers />
+        </Suspense>
 
-export default Computers;
+        <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
