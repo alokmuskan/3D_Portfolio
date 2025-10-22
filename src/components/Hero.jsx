9a0d87a3853f9be4350dcac3575ab7f47e5163df
alok raj <alokraj1319@gmail.com>
2025-10-22 22:31:07 +0530

    completed styling name along with a small intro in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,6 +17,17 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40
           violet-gradient" />
         </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText}
+          text-white`}>Hi, I'm <span 
+          className="text-[#915eff]">Alok Raj</span></h1>
+          <p className={`${styles.heroSubText} mt-2
+          text-white-100`}>
+            I develop 3D visuals, user <br className="sm:block hidden"/>
+            interfaces and web applications.
+          </p>
+        </div>
       </div>
     </section>
   );
