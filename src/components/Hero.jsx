372b7b08128a3cd4dfb7b6d60678c9d0d4bd8c87
alok raj <alokraj1319@gmail.com>
2025-10-22 22:08:57 +0530

    added some more better styling to this section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,16 @@ import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
     <section className="relative w-full h-screen
-    mx-auto">Hero</section>
+    mx-auto">
+      <div className={`${styles.paddingX} absolute
+      insert-0 top-[120px] max-w-7xl mx-auto flex 
+      flex-row items-start gap-5 `}> 
+        <div className="flex flex-col 
+        justify-center items-center mt-5">
+          
+        </div>
+      </div>
+    </section>
   );
 };
 
