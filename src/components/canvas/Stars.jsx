aa95c37f6952f7ed110f2f4bb89aa0badf920d4f
alok raj <alokraj1319@gmail.com>
2025-10-24 01:39:36 +0530

    completed the adding of stars to the background to the contacts page

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,8 +5,12 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  
+  // density of stars
+  const [sphere] = useState(() => random.inSphere(new Float32Array
+    (5000), { radius: 1.2 }));
 
+  // allows to rotate something frame by frame
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
@@ -14,7 +18,8 @@ const Stars = (props) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} 
+      stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color='#f272c8'
