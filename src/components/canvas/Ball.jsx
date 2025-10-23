575f97c791a0b587af43763a6bb811b32a623ff0
alok raj <alokraj1319@gmail.com>
2025-10-23 22:05:09 +0530

    completed with the tech section and also the styling of balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,7 +10,26 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1}
+    floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial 
+        color="#fff8eb"
+        polygonOffset
+        polygonOffsetFactor={-5}
+        floatShading
+        />
+        <Decal 
+          position={[0, 0, 1]}
+          rotation={[ 2 * Math.PI, 0, 6.28]}
+          floatShading
+          map={decal}
+        />
+      </mesh>
+    </Float>
   )
 }
 
